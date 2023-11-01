4895ceec3b1155bc2d16640b5267d6427d9af804
snehay1903 <149564915+snehay1903@users.noreply.github.com>
2023-11-01 12:55:02 +0000

    variables done

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,17 @@
+const accountId = 23456
+let accountEmail = "sneha@gmail.com"
+var accountPassword = "sneha123"
+accountCity = "Jaipur"
+let accountState;
+
+// accountId = 12398
+
+accountEmail = "snehay@1903@gmail.com"
+accountPassword = "chai123"
+accountCity = "Mumbai"
+
+console.table([accountEmail,accountPassword,accountCity,accountState])
+
+
+
+console.log(accountId);
